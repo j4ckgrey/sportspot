cae92e5d67d5f6cf0256c8bf0eb9b3462fcd35f6
jack grey <j4ckgrey@gmail.com>
2023-06-05 16:21:55 +0200

    simple view for index and show, 2 controller methods created in venues controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_131044) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_141415) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_131044) do
     t.bigint "club_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["club_id"], name: "index_venues_on_club_id"
   end
 
