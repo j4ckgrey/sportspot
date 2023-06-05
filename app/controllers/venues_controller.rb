e118c50fe875411213677652e47a9d4b2274f5b4
jack grey <j4ckgrey@gmail.com>
2023-06-05 23:05:48 +0200

    pundit, clubs and venues compelte

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,27 @@
 class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+    @venues = policy_scope(Venue)
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def new
+    @venue = Venue.new
+    authorize @venue
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user = current_user
+    authorize @venue
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :category, :club_id)
+  end
 end
