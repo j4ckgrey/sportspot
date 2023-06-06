dd348a3abdc36fe6bfcb13f9c2ddac424d54d6d1
Tim Schulze Sölde <tim@rotthege.com>
2023-06-06 12:01:15 +0200

    generated t

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+puts 'Creating 5 users...'
+for i in 1..5 do
+  user = User.new(
+    email: "email#{i}@user.com",
+    first_name: "User#{i}",
+    last_name: "User#{i}",
+    password: "Password#{i}",
+    zip_code: "5067#{i}",
+    city: "City#{i}",
+    street: "Street#{i}",
+    phone_number: "0173 00000#{i}"
+  )
+  user.save!
+end
+
+puts 'Creating 5 clubs with 5 venues each...'
+for i in 1..5 do
+  club = Club.new(
+    email: "email#{i}@club.com",
+    name: "Club#{i}",
+    password: "Password#{i}",
+    zip_code: "5067#{i}",
+    city: "City#{i}",
+    street: "Street#{i}",
+    phone_number: "0173 00000#{i}"
+  )
+  club.save!
+  for v in 1..5 do
+    venue = Venue.new(
+      name: "Venue#{v}",
+      category: "categroy1",
+      description: "Description#{v}",
+      club_id: club.last
+    )
+    venue.save!
+  end
+end
+
+puts 'Finished!'
