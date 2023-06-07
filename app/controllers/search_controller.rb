5005604da050769bceb9fb0cc6fd994dd4f154d5
ishmam alam <ishmam.alam01@gmail.com>
2023-06-07 11:29:52 +0200

    search implemented

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,25 +1,15 @@
 class SearchController < ApplicationController
   def index
-    @query = params[:query]
+    @query = params[:query].to_i
     @category = params[:category]
-    @results = search_results(@query, @category)
+    @results = policy_scope(search_results(@query, @category))
   end
-
-  # def search_results(query, category)
-  #   # table = table_name.constantize
-  #   if category == "All"
-  #     Post.where("zip_code LIKE?", "%#{query}")
-  #   else
-  #     Post.where("zip_code LIKE ? AND category = ?", "%#{query}", category)
-  #   end
-  # end
 
   def search_results(query, category)
     if category == "All"
-      Post.joins(:venues).where("clubs.zip_code LIKE ?", "%#{query}%")
+      Club.joins(venues: :club).where("CAST(clubs.zip_code AS VARCHAR) LIKE ?", "%#{query}%")
     else
-      Post.joins(:venues).where("clubs.zip_code LIKE ? AND venues.category = ?", "%#{query}%", category)
+      Club.joins(:venues).where("clubs.zip_code = ? AND venues.category = ?", query.to_i, category)
     end
   end
-
 end
