cb61b8da18dece8e1dcc1c767915e0b6e7847484
Tim Schulze Sölde <tim@rotthege.com>
2023-06-08 16:17:29 +0200

    finished the dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "/dashboard", to: "pages#dashboard"
+  # get "/own", to: 'clubs#own'
 
   resources :clubs do
     resources :venues, only: %i[new create]
