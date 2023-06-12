edc033a446ad3373349e5616eda5e7fca05ea91b
Tim Schulze Sölde <tim@rotthege.com>
2023-06-12 09:56:57 +0200

    added price migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_131742) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_072741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_131742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.float "price"
     t.index ["club_id"], name: "index_venues_on_club_id"
   end
 
