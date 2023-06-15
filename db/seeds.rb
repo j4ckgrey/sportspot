37b5dfd58198748811e7cf0b689b6999ce37269a
Tim Schulze Sölde <tim@rotthege.com>
2023-06-15 16:05:53 +0200

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,20 @@
-User.delete_all
-Club.delete_all
+Chatroom.delete_all
+Review.delete_all
+Booking.delete_all
 Venue.delete_all
+Club.delete_all
+User.delete_all
 
 clubs_names = ["Kampfsport Center Köln", "Kautz Sportcenter", "Boulderplanet", "Kletterfabrik Köln", "K11 | Bouldering in South City", "Stuntwerk Köln", "Kegelclub - Die Teddybären", "City-Bowling Köln", "WDR Volleyball", "SC Janus e. V.", "ESV Olympia Köln - Fußball", ]
 clubs_zip_codes = ["50823", "50939", "50825", "50825", "50678", "51063", "50676", "50674", "50676", "50674", "50739"]
 clubs_streets = ["Venloer Str. 182", "Rhöndorfer Str. 10-13", "Oskar-Jäger-Straße 143H", "Oskar-Jäger-Straße 173", "Kyllstraße 11", "Schanzenstraße 6-20", "Agrippastraße 6", "Moselstraße 44", "Am Pantaleonsberg 3", "Hohenstaufenring 42", "Lämmerstr."]
 clubs_phone_numbers = "123456789"
 clubs_emails = "something@address.de"
-description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus consequat velit, in tincidunt orci ornare at. Nunc elit mauris, luctus non dapibus feugiat, bibendum iaculis odio. Morbi orci ante, egestas a risus non, mattis bibendum nulla. Mauris et dolor quis nibh pulvinar pharetra in commodo nisi. Fusce vel arcu non odio bibendum fermentum at nec eros. Vivamus vitae feugiat mi. Nam id dolor ac dolor tristique dignissim. Morbi et congue quam. Morbi molestie vitae tortor sit amet suscipit."
+description = "Welcome to the club, the premier sport club dedicated to nurturing talent and fostering a passion for sports! Located in the heart of the city, Apex Athletics is a vibrant and inclusive community where athletes of all ages and skill levels come together to train, compete, and grow.
+Our state-of-the-art facility boasts top-notch amenities, including well-equipped gyms, cutting-edge training equipment, and meticulously maintained playing fields. Whether you're a beginner looking to develop fundamental skills or a seasoned athlete aiming for the highest level of performance, we have programs tailored to suit your needs.
+At Apex Athletics, we offer a wide range of sports disciplines to cater to diverse interests. From soccer, basketball, and tennis to swimming, gymnastics, and martial arts, our experienced coaches provide expert guidance and create a supportive environment to help you achieve your goals. We emphasize holistic development, focusing not only on physical prowess but also on character-building, teamwork, and sportsmanship.
+For our younger athletes, we have a comprehensive youth development program that introduces them to various sports, fostering a love for an active lifestyle and instilling values of discipline and resilience. Our adult leagues and recreational programs offer opportunities for friendly competition and social engagement, ensuring a fulfilling experience for athletes of all ages.
+Join the Apex Athletics family today and unlock your full athletic potential. Experience the thrill of pushing boundaries, making lifelong friends, and creating unforgettable memories. Get ready to reach new heights with us at Apex Athletics!"
 user = User.create(email: "jack@jack.jack", password: "jackjack", first_name: "Jack", last_name: "Grey", zip_code: 42929, city: "Wermelskirchen", street: "Eich 34", phone_number: 123456789, roles: ['owner'])
 clubs_names.each_with_index do |clubname, index|
   club = Club.new(name: clubname)
@@ -23,6 +30,8 @@ clubs_names.each_with_index do |clubname, index|
   club.save
 end
 clubs = Club.all
+description ="Welcome to the venue, a breathtaking sports venue that embodies the epitome of athletic excellence and fan engagement. Nestled amidst the picturesque surroundings, this state-of-the-art facility stands as a testament to the unifying power of sports and the thrill of competition.
+it is a colossal architectural marvel, seamlessly blending modern design with a deep appreciation for the local environment. Its sweeping curves and sleek lines pay homage to the nearby river, with large windows strategically placed to offer panoramic views of the surrounding landscape."
 Venue.create(name: "MartialArts", category: "MartialArts", description: description, price: 17.99, club: clubs[0])
 Venue.create(name: "Boulderplanet", category: "Bouldering", description: description, price: 15.99, club: clubs[2])
 Venue.create(name: "Kletterfabrik Köln", category: "Bouldering", description: description, price: 70.99, club: clubs[3])
