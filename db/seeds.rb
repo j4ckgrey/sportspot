d7d2e290799ddd6e344d4473165f193f79cd2ad6
jack grey <j4ckgrey@gmail.com>
2023-06-15 16:54:02 +0200

    messages delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 require "open-uri"
+Message.delete_all
 Chatroom.delete_all
 Review.delete_all
 Booking.delete_all
 User.delete_all
 Club.delete_all
 Venue.delete_all
+
 
 basketball = URI.open("https://images.unsplash.com/photo-1519766304817-4f37bda74a26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")
 tennis = URI.open("https://images.unsplash.com/photo-1580763850522-504d40a05c50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80")
